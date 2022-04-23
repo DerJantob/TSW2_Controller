8e01f24ebb4ce3187441d2aeb1c73648767eb39c
Jannik Wiechers <Schranke50@gmail.com>
2022-04-24 00:47:11 +0200

    WasIstNeu

--- TSW2_Controller/FormWasIstNeu.cs
+++ TSW2_Controller/FormWasIstNeu.cs
@@ -167,11 +167,13 @@ namespace TSW2_Controller
                 {
                     changelog.Add("- Windows 11 kompatibel");
                     changelog.Add("- Hinzugefügt: Joysticks können jetzt während das Programm läuft angeschlossen werden");
+                    changelog.Add("- Verbessert: Alte Logdateien werden automatisch gelöscht");
                 }
                 else
                 {
                     changelog.Add("- Windows 11 compatible");
                     changelog.Add("- Added: Joysticks can now be plugged in while program is running");
+                    changelog.Add("- Improved: Old logfiles get deleted automatically");
                 }
 
                 changelog.Add("----------------------------------------");
